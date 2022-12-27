121d0242c7c4d50a23d9ac079f933a7b6f2643a5
ziedshiro <krirk.k@ku.th>
2022-12-28 01:14:40 +0700

    Update Unit_Test.py

--- Unit_Test.py
+++ Unit_Test.py
@@ -50,9 +50,17 @@ def long_number_in_list():
     merge(collection_1,collection_2)
 
 def many_value_in_list():
-    collection_1=[10000000,1200000000,2000000000000,2200000000000,2000000000000]
-    collection_2=[70000000,270000000,6370000000,66270000000,223170000000]
+    collection_1=[1,2,3,5,6,8,7,9,10,11,12,15,17,20]
+    collection_2=[10,21,33,52,62,83,74,91,100,101,112,115,117,230]
+    merge(collection_1,collection_2)
+
+def negative_number_in_list():
+    collection_1=[-19,-12,-3,-2]
+    collection_2=[-10,-7,-5,-4]
     merge(collection_1,collection_2)
 
 if __name__ == '__main__':
     Duplicate_Number_in_list()
+    long_number_in_list()
+    many_value_in_list()
+    negative_number_in_list()
